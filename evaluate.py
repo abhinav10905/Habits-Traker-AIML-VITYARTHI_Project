1159d1cc61c665a7cbd0ab349721e0bf5f18886b
Abhinav Jain <abhinavjain10905@gmail.com>
2026-03-20 22:20:55 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,5 @@
 from sklearn.metrics import accuracy_score, classification_report
 def evaluate_model(model, X_test, y_test):
-    """
-    Print basic evaluation metrics for the model.
-    """
     y_pred = model.predict(X_test)
     a = accuracy_score(y_test, y_pred)
     print(f"\nModel accuracy: {a:.2f}")
